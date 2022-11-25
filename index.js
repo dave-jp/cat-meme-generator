d605f72b3b38efcec2d127d5ae07ad5506437352
David Preece <113293951+dave-jp@users.noreply.github.com>
2022-11-25 14:39:59 +0000

    fixed target id to only target the one selected

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', highlightSelectedOption)
+
+// create a function that changes the colour of the radio parent that has been selected
+function highlightSelectedOption(e) {
+    console.log(e.target.id)
+}
+
 // function iterates over object and pulls emotion tags only if they are not already shown
 function getEmotionsArray() {
     const emotionsArray = []
     for (let emotions of catsData){
-        for (let catEmotion of emotions.emotionTags){
+        for (let catEmotion of emotions.emotionTags){ 
             if (!emotionsArray.includes(catEmotion)){
                 emotionsArray.push(catEmotion)
             }
@@ -17,18 +24,18 @@ function getEmotionsArray() {
 
 // render the emotions out on to the page from getEmotionsArray
 function renderEmotionsArray() {
+    let emotionArray = ''
     let emotions = getEmotionsArray()
     for (let emotion of emotions){
-    emotionRadios.innerHTML += `
+    emotionArray += `
     <div class="radio">
-        <label for="${emotion}" class="emotion-radio">${emotion}</label>
-        <input type="radio" id="${emotions} class="radio-button" name="emotion-radios">
+        <label for="${emotion}">${emotion}</label>
+        <input type="radio" id="${emotion}" value="${emotion}" name="emotion-radios">
     </div>
     `
     }
+    emotionRadios.innerHTML += emotionArray
 }
 
-// create a function that changes the colour of the radio parent that has been selected
 
 renderEmotionsArray()
-
